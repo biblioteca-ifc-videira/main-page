7b418eeb05f7c2116e42f37f084e814c4e915085
rocky <rocky@localhost.localdomain>
2023-12-05 20:13:25 -0300

    Updates

---
+++ precache-manifest.c6d5dee38ccb80511432d398dcdfb8c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c167529da29f6afc5b53172fce6c8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e406f32066f125718c76",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "66808c1bddaea548fb07",
+    "url": "/static/js/2.9a4da608.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.9a4da608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626928988efd4deb137e",
+    "url": "/static/js/3.ec90a615.chunk.js"
+  },
+  {
+    "revision": "e406f32066f125718c76",
+    "url": "/static/js/main.36de3345.chunk.js"
+  },
+  {
+    "revision": "0146fae33da06f6519e7",
+    "url": "/static/js/runtime-main.9820335c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
